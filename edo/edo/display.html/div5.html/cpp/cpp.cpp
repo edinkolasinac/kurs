b250f7f95f008b852fbf539e660c22201c36224a
edin <edokolasinac@hotmail.com>
2022-03-03 23:25:23 +0100

    edo

---
+++ edo/edo/display.html/div5.html/cpp/cpp.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int myNum = 5;            // Integer (whole number without decimals)
+double myFloatNum = 5.99; // Floating point number (with decimals)
+char myLetter = 'D';      // Character
+string myText = "Hello";  // String (text)
+bool myBoolean = true;
