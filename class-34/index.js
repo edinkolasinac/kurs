b830080ef7aeb1402f2c89da2f9c6d6169dc7365
edin <edokolasinac@hotmail.com>
2022-05-17 23:08:52 +0200

    fetch extra

--- class-34/index.js
+++ class-34/index.js
@@ -1,14 +1,58 @@
 const fetch = require("node-fetch");
-const fn = async (args) => {
-    const author = args[0];
+const randomQuoteFromAuthor = async (author) => {
+    try {
+        const checkedAuthor = await authorCheck(author);
 
-    const response = await fetch(
-        `https://api.quotable.io/random${author ? `?author=${author}` : ""}`
-    );
-    const result = await response.json();
+        if (!checkedAuthor) {
+            return "That author doesn't exist";
+        }
 
-    console.log(result);
-    console.log(result.content + "; author: " + result.author);
+        const response = await fetch(
+            `https://api.quotable.io/random${checkedAuthor ? `?author=${checkedAuthor}` : ""
+            }`
+        );
+        const result = await response.json();
+
+        return result.content + ", Author: " + result.author;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-fn("Buddha");
+const authorCheck = async (author) => {
+    try {
+        const response = await fetch(
+            `https://api.quotable.io/search/authors?query=${author}`
+        );
+        const result = await response.json();
+
+        if (!result.count) {
+            return false;
+        }
+        return result.results[0].name;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const getQuotes = async (page, sortBy, order) => {
+    try {
+        const paramsExist = page || sortBy || order;
+        const response = await fetch(
+            `https://api.quotable.io/quotes${paramsExist ? "?" : ""}${page ? `page=${page}` : ""
+            }${page ? "&" : ""}${sortBy ? `sortBy=${sortBy}` : ""}${page || sortBy ? "&" : ""
+            }${order ? `order=${order}` : ""}`
+        );
+        const result = await response.json();
+        return result.results;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+getQuotes(95, "content", "desc").then((result) => {
+    for (let i = 0; i < result.length; i++) {
+        const element = result[i];
+        console.log(element.content + " by: " + element.author);
+    }
+});
