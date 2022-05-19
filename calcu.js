4b7ab42535e4b5fe4ae51a8f675e143c4e97b8ef
Edo <edokolasina@hotmail.com>
2022-05-19 13:05:11 +0200

    cacth

--- calcu.js
+++
@@ -1,4 +0,0 @@
-function sum(a, b) {
-    return a + b;
-}
-const a = 10;
--- class-34/index.js
+++ class-34/index.js
@@ -1,14 +1,33 @@
 const fetch = require("node-fetch");
-const fn = async (args) => {
-    const author = args[0];
+const fn = async (author) => {
+    try {
+        const response = await fetch(
+            `https://api.quotable.io/random${author ? `?author=${author}` : ""}`
+        );
+        const result = await response.json();
+        console.log(result);
 
-    const response = await fetch(
-        `https://api.quotable.io/random${author ? `?author=${author}` : ""}`
-    );
-    const result = await response.json();
-
-    console.log(result);
-    console.log(result.content + "; author: " + result.author);
+        console.log(result.content + "; author: " + result.author);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-fn("Buddha");
+const authorCheck = async (author) => {
+    try {
+        const response = await fetch(
+            `https://api.quotable.io/search/authors?query=${author}`
+        );
+        const result = await response.json();
+        console.log(result);
+        if (result.count < 1) {
+            return false;
+        }
+
+        return result.results[0].name;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+authorCheck("Adam").then((value) => console.log(value));
