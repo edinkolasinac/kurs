821b78180c26d5b941469cb32b1a5a4df25bb38b
edin <edokolasinac@hotmail.com>
2022-06-07 22:19:04 +0200

    more fetching

--- v2/class37/script.js
+++ v2/class37/script.js
@@ -1,20 +1,86 @@
-const credits = ((num) => {
-    let credits = num;
-    console.log(`broj vasih kredita je jednak: ${credits}`);
-    return () => {
-        credits -= 1;
-        if (credits > 0) console.log(`igrate igru broj kredita: ${credits} preostala credita(a)`);
-        if (credits < 0) console.log(`nemas te vise ${credits} presotalo ti je `);
+const getQuotes = async (page = 1) => {
+    try {
+      const response = await fetch(
+        `https://api.quotable.io/quotes?${page ? `page=${page}` : ""}`
+      );
+  
+      const result = await response.json();
+  
+      return result.results;
+    } catch (error) {
+      console.log(error);
     }
-})(3);
-credits();
-credits();
-
-credits();
-credits();
-
-credits();
-
-
-
-
+  };
+  
+  let page = 1;
+  const contentDiv = document.getElementById("content");
+  const pageInput = document.getElementById("page");
+  
+  // const createCard = (author, quote) => {
+  //   return `<div
+  //  style="
+  //    width: 400px;
+  //    min-height: 100px;
+  //    border: 1px solid black;
+  //    margin: 10px;
+  //    padding: 20px;
+  //  "
+  // >
+  //  <p>
+  //    ${quote}
+  //  </p>
+  //  <p>By: ${author}</p>
+  // </div>`;
+  // };
+  
+  // getQuotes().then((result) => {
+  //   result.forEach((quote) => {
+  //     console.log(contentDiv.innerHTML);
+  //     contentDiv.innerHTML =
+  //       contentDiv.innerHTML + createCard(quote.author, quote.content);
+  //   });
+  //   //   contentDiv.appendChild();
+  // });
+  
+  const createCardNode = (author, quote) => {
+    const div = document.createElement("div");
+    div.style = ` width: 400px;
+         min-height: 100px;
+         border: 1px solid black;
+         margin: 10px;
+         padding: 20px;
+       `;
+  
+    const p = document.createElement("p");
+    p.textContent = quote;
+  
+    div.appendChild(p);
+  
+    const p2 = document.createElement("p");
+    p2.textContent = `By: ${author}`;
+  
+    div.appendChild(p2);
+  
+    return div;
+  };
+  
+  // getQuotes().then((result) => {
+  //   result.forEach((quote) => {
+  //     const newCard = createCardNode(quote.author, quote.content);
+  //     contentDiv.appendChild(newCard);
+  //   });
+  // });
+  
+  const clickable = () => {
+    getQuotes(pageInput.value).then((result) => {
+      contentDiv.innerHTML = "";
+      result.forEach((quote) => {
+        const newCard = createCardNode(quote.author, quote.content);
+        contentDiv.appendChild(newCard);
+      });
+    });
+  };
+  
+  const button = document.getElementById("click");
+  
+  button.addEventListener("click", clickable);
