43a7227f8f04294ed54fe70f98381b647dbe0fe5
edin <edokolasinac@hotmail.com>
2022-06-14 17:41:25 +0200

    pokemonapi

--- v2/class37/script.js
+++ v2/class37/script.js
@@ -1,86 +1,106 @@
 const getQuotes = async (page = 1) => {
-    try {
-      const response = await fetch(
-        `https://api.quotable.io/quotes?${page ? `page=${page}` : ""}`
-      );
-  
-      const result = await response.json();
-  
-      return result.results;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  let page = 1;
-  const contentDiv = document.getElementById("content");
-  const pageInput = document.getElementById("page");
-  
-  // const createCard = (author, quote) => {
-  //   return `<div
-  //  style="
-  //    width: 400px;
-  //    min-height: 100px;
-  //    border: 1px solid black;
-  //    margin: 10px;
-  //    padding: 20px;
-  //  "
-  // >
-  //  <p>
-  //    ${quote}
-  //  </p>
-  //  <p>By: ${author}</p>
-  // </div>`;
-  // };
-  
-  // getQuotes().then((result) => {
-  //   result.forEach((quote) => {
-  //     console.log(contentDiv.innerHTML);
-  //     contentDiv.innerHTML =
-  //       contentDiv.innerHTML + createCard(quote.author, quote.content);
-  //   });
-  //   //   contentDiv.appendChild();
-  // });
-  
-  const createCardNode = (author, quote) => {
-    const div = document.createElement("div");
-    div.style = ` width: 400px;
-         min-height: 100px;
-         border: 1px solid black;
-         margin: 10px;
-         padding: 20px;
-       `;
-  
-    const p = document.createElement("p");
-    p.textContent = quote;
-  
-    div.appendChild(p);
-  
-    const p2 = document.createElement("p");
-    p2.textContent = `By: ${author}`;
-  
-    div.appendChild(p2);
-  
-    return div;
-  };
-  
-  // getQuotes().then((result) => {
-  //   result.forEach((quote) => {
-  //     const newCard = createCardNode(quote.author, quote.content);
-  //     contentDiv.appendChild(newCard);
-  //   });
-  // });
-  
-  const clickable = () => {
-    getQuotes(pageInput.value).then((result) => {
-      contentDiv.innerHTML = "";
-      result.forEach((quote) => {
-        const newCard = createCardNode(quote.author, quote.content);
-        contentDiv.appendChild(newCard);
-      });
+  try {
+    const response = await fetch(
+      `https://api.quotable.io/quotes?${page ? `page=${page}` : ""}`
+    );
+
+    const result = await response.json();
+
+    return result.results;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+let page = 1;
+const contentDiv = document.getElementById("content");
+const pageInput = document.getElementById("page");
+
+// const createCard = (author, quote) => {
+//   return `<div
+//  style="
+//    width: 400px;
+//    min-height: 100px;
+//    border: 1px solid black;
+//    margin: 10px;
+//    padding: 20px;
+//  "
+// >
+//  <p>
+//    ${quote}
+//  </p>
+//  <p>By: ${author}</p>
+// </div>`;
+// };
+
+// getQuotes().then((result) => {
+//   result.forEach((quote) => {
+//     console.log(contentDiv.innerHTML);
+//     contentDiv.innerHTML =
+//       contentDiv.innerHTML + createCard(quote.author, quote.content);
+//   });
+//   //   contentDiv.appendChild();
+// });
+
+const createCardNode = (author, quote) => {
+  const div = document.createElement("div");
+  div.style = ` width: 400px;
+       min-height: 100px;
+       border: 1px solid black;
+       margin: 10px;
+       padding: 20px;
+     `;
+
+  const p = document.createElement("p");
+  p.textContent = quote;
+
+  div.appendChild(p);
+
+  const p2 = document.createElement("p");
+  p2.textContent = `By: ${author}`;
+
+  div.appendChild(p2);
+
+  return div;
+};
+
+// getQuotes().then((result) => {
+//   result.forEach((quote) => {
+//     const newCard = createCardNode(quote.author, quote.content);
+//     contentDiv.appendChild(newCard);
+//   });
+// });
+
+const clickable = () => {
+  getQuotes(pageInput.value).then((result) => {
+    contentDiv.innerHTML = "";
+    result.forEach((quote) => {
+      const newCard = createCardNode(quote.author, quote.content);
+      contentDiv.appendChild(newCard);
     });
-  };
+  });
+};
+
+const button = document.getElementById("click");
+
+button.addEventListener("click", clickable);
+
+
+
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 2000);
+    });
+  }
   
-  const button = document.getElementById("click");
+  async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: "resolved"
+  }
   
-  button.addEventListener("click", clickable);
+  asyncCall();
+  
---
+++ v2/vezba/vezba2/script.js
@@ -0,0 +1,11 @@
+fetch(' https://pokeapi.co/api/v2/pokemon/1')
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
+if (res.ok) {
+    console.log(edo);
+
+} else {
+
+}
+
